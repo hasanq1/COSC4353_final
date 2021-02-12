a2cd3c720d558094a6611667cf27b9f77112ca9a
Hasan Q <hasanq36@gmail.com>
2021-02-12 00:05:17 -0600

    backendupdate3.0

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,28 +1,30 @@
 import asyncHandler from 'express-async-handler'
+import generateToken from '../utils/generateToken.js'
 import User from '../models/userModel.js'
 
 
 
 const authUser = asyncHandler(async (req,res) => {
     const {email, password } = req.body
-
-
     const user = await User.findOne({email})
-
     if(user && (await user.matchPassword(password))){
         res.json({
             _id: user._id,
             name: user.name,
             email: user.email,
             isAdmin: user.isAdmin,
-            token: null
+            token: generateToken(user._id)
         })
     } else {
         res.status(401)
         throw new Error()
     }
 })
+const getUserProfile = asyncHandler(async (req,res) => {
+    res.send('Success')
+})
 
 
-export { authUser }
+
+export { authUser, getUserProfile }
 

a0e21c9fdbc4d5288d56d5f6e4e0b18903093112
Hasan Q <hasanq36@gmail.com>
2021-02-11 23:05:08 -0600

    backendupdate2.0

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,11 @@ import dotenv from 'dotenv'
 import colors from 'colors'
 import {notFound, errorHandler} from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
+
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+
+
 
 dotenv.config()
 
@@ -11,16 +15,14 @@ connectDB()
 
 const app = express()
 
-app.use((req,res,next) => {
-   console.log(req.originalUrl)
-   next() 
-})
+app.use(express.json())
 
 app.get('/',(req,res) =>{
     res.send('API is running...')
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
 

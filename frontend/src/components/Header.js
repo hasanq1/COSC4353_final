cfd0beff416ad364800b55c6b0d244642f8b2e2c
Hasan Q <hasanq36@gmail.com>
2021-02-15 01:59:08 -0600

    frontend+backFINAL2.0

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -20,7 +20,7 @@ const Header = () => {
         <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
           <Container>
             <LinkContainer to='/'>
-              <Navbar.Brand>Hasan_Ecommerce_tutorial_proj </Navbar.Brand>
+              <Navbar.Brand>Hasan's ecommerce guided project </Navbar.Brand>
             </LinkContainer>
             <Navbar.Toggle aria-controls='basic-navbar-nav' />
             <Navbar.Collapse id='basic-navbar-nav'>
